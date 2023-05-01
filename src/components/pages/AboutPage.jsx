6a06fe56b97a15a7b23c348c60e4f1a4f1778ac0
Rodrigo Castro <castro.rl@gmail.com>
2023-05-01 11:52:54 -0300

    cleaning code after plaoying with router

--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -1,16 +1,11 @@
 import Card from '../shared/Card';
-import { Link } from 'react-router-dom';
-import { FaHouseUser } from 'react-icons/fa';
-// import { useParams } from 'react-router-dom';
-const AboutPage = ({handleAboutClick}) => {
-  // const params = useParams();
+const AboutPage = () => {
   return (
     <Card>
       <div className='about'>
         <h1>About This Project</h1>
         <p>This is a React app to leave feedback for a product or service.</p>
         <p>Version: 1.0.0</p>
-        {/* <p>{params.id}</p> */}
 
       </div>
     </Card>
